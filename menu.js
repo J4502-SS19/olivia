5a7984826d39adef82d70d349840bae0c5f79eea
Olivia Garrett <ojg5g8@mail.missouri.edu>
2019-04-15 16:56:51 -0500

    java menu

--- menu.js
+++ menu.js
@@ -1,12 +1,12 @@
 
 $(document).ready(function() {
   $('.menu-trigger').click(function() {
-    $('div ul').slideToggle(500);
+    $('nav ul').slideToggle(500);
   });//end slide toggle
 
   $(window).resize(function() {
 	   if (  $(window).width() > 700 ) {
-		$('div ul').removeAttr('style');
+		$('nav ul').removeAttr('style');
 	 }
 	});//end resize
 });//end ready
